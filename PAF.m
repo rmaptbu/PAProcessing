227496d7c8a1c94e92234d5a15fe70fbb5091589
Thore Bücking <thore@buecking.me>
2015-10-26 16:53:51 +0000

    went back to using the sublplot environment.. :(

--- PAF.m
+++ PAF.m
@@ -275,41 +275,22 @@ end
      'symmetric', 'post');
  %% create plot
  figure;
- y_pos=0;
- x_pos=0;
- ncol=4;
- nrow=3;
- plotsizeX=0.6/ncol;
- plotsizeY=0.5/nrow;
- for i=1:number_of_files;     
-     if ~rem(i-1,ncol);y_pos=y_pos+1;x_pos=0; end;
-     ax1=axes('Position',...
-         [0.1+x_pos*(plotsizeX+0.045) 1-(plotsizeY+0.1)*y_pos plotsizeX plotsizeY],...
-         'XTickLabel','',...
-         'YTickLabel','shift (ns)');
-     ax2=axes('Position',ax1.Position);
-     plot(ax1,maxprofile_interp(:,i), 'Color', [0, 0, 0]);hold on;
-     plot(ax2,shift_all(i,:)', 'Color', [1, 0, 0]);
-     set(gca, 'Color', 'None','XColor','r','YColor','r',...
-         'XAxisLocation','top',...
-         'YAxisLocation','right',...
-         'Box','off',...
-         'XLim', ax1.XLim,'YLim',ax1.YLim,... 'XTick',[],'YTick',[],...
-         'XColor', [0 0 0], 'YColor', [0 0 0]);
-     S=strrep(names(i),'_','\_');
+ 
+for i=1:number_of_files; subplot(4,4,i);     
+     plot(maxprofile_interp(:,i), 'Color', [0, 0, 0]);
+     hold on
+     plot(shift_all(i,:)', 'Color', [1 0 0]);
+     S=strrep(names(i),'_',' _');
      S=S{1};
-     S=[S(1:20),'\ xcorr', num2str(shift_all(i,1))];
+     S=[S(1:20),'  xcorr', num2str(shift_all(i,1))];
      title(S);
-     x_pos=x_pos+1;
- end
- i=i+1;
- if ~rem(i-1,ncol);y_pos=y_pos+1;x_pos=0; end;
- ax1=axes('Position',...
-         [0.1+x_pos*(plotsizeX+0.1) 1-(plotsizeY+0.1)*y_pos plotsizeX*2 plotsizeY],...
-         'XTickLabel','',...
-         'YTickLabel','shift (ns)');
- plot(pressure(:,1),'Color',[0 0 0]);hold on;
- for i=1:jmax-1;
-     plot(ax1,N+i*q:N+i*q+49,pressure(N+i*q:N+i*q+49,1),...
+     hold off;
+end
+subplot(4,4,i+1:i+2)
+plot(pressure(:,1),'Color',[0 0 0]);hold on
+ for i=1:jmax-1;     
+     box on
+     plot(N+i*q:N+i*q+49,pressure(N+i*q:N+i*q+49,1),...
          'LineWidth',2,'Color',[i/jmax,.5-i/(jmax*2),1-i/jmax]);
  end;hold off;
+
